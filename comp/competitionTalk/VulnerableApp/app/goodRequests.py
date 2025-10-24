d427f94ab48288c7f44565747c0472513478f5e4
whyat2 <141190141+whyat2@users.noreply.github.com>
2025-10-24 18:42:52 -0500

    fix timers

--- comp/competitionTalk/VulnerableApp/app/goodRequests.py
+++ comp/competitionTalk/VulnerableApp/app/goodRequests.py
@@ -12,21 +12,21 @@ from priceEncrypted import check_price_column_encryption
 output_dir = "test_results"
 os.makedirs(output_dir, exist_ok=True)
 
-time.sleep(15)
+time.sleep(30)
 add_product("Item0", 12.5)
 add_product("Item01", 12.5)
 add_product("Item02", 12.5)
 
-time.sleep(30)
+time.sleep(450)
 add_product("Item1", 12.5)
 getUnencryptedPass("./test_results/getNotHashed.txt")
 add_product("Item2", 13.5)
 
-time.sleep(30)
+time.sleep(450)
 manage_products("./test_results/modifyNonsense.txt")
 add_product("Item3", 14.5)
 
-time.sleep(30)
+time.sleep(450)
 check_price_column_encryption("./test_results/encrypt.txt")
 
 time.sleep(2)
